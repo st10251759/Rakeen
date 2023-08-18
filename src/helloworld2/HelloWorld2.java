e1c11196572e435956b810b87e8db4257753c12e
st10251759 <st10251759@vcconnect.edu.za>
2023-08-18 13:27:51 +0000

    First Commit

---
+++ src/helloworld2/HelloWorld2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class HelloWorld2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("Hello World!!! Rakeen and Allissa");
+    }
+    
+}
